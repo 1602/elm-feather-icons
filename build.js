52d75eaf5cf4a65098593491f69dff8921178664
Anatoliy <mail@anatoliy.in>
2018-12-31 13:35:43 +0000

    Use .contents of icon rather than .toSvg

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const feather = require('feather-icons');
 
 require('./source-builder.js')
     .Headless
-    .worker(Object.keys(feather.icons).map(x => ([ x, feather.icons[x].toSvg()])))
+    .worker(Object.keys(feather.icons).map(x => ([ x, feather.icons[x].contents])))
     .ports
     .output
     .subscribe(x => console.log(x));
